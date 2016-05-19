95e1908dc7d5105e780aef74076b792ff910297e
Jamie Iles <jamie@jamieiles.com>
2016-05-19 23:33:12 +0100

    emulate: remove useless comments that just take up screen space.

--- sim/Emulate.cpp
+++ sim/Emulate.cpp
@@ -28,10 +28,6 @@ public:
 
     void reset();
 private:
-    uint8_t fetch_byte();
-    //
-    // mov
-    //
     void mov88();
     void mov89();
     void mov8a();
@@ -46,174 +42,84 @@ private:
     void mova3();
     void mov8e();
     void mov8c();
-    //
-    // push
-    //
     void pushff();
     void push50_57();
     void pushsr();
-    //
-    // pop
-    //
     void pop8f();
     void pop58_5f();
     void popsr();
-    //
-    // xchg
-    //
     void xchg86();
     void xchg87();
     void xchg90_97();
-    //
-    // in
-    //
     void ine4();
     void ine5();
     void inec();
     void ined();
-    //
-    // out
-    //
     void oute6();
     void oute7();
     void outee();
     void outef();
-    //
-    // xlat
-    //
     void xlatd7();
-    //
-    // lea
-    //
     void lea8d();
-    //
-    // lds
-    //
     void ldsc5();
-    //
-    // les
-    //
     void lesc4();
-    //
-    // lahf
-    //
     void lahf9f();
-    //
-    // sahf
-    //
     void sahf9e();
-    //
-    // pushf
-    //
     void pushf9c();
-    //
-    // popf
-    //
     void popf9d();
-    //
-    // add / adc
-    //
     void add_adc_sub_sbb_cmp_80();
     void add_adc_sub_sbb_cmp_81();
     void add_adc_sub_sbb_cmp_82();
     void add_adc_sub_sbb_cmp_83();
-    //
-    // add
-    //
-    template <typename T>
-    std::pair<uint16_t, T> do_add(uint16_t v1, uint16_t v2,
-                                  uint16_t carry_in=0);
     void add00();
     void add01();
     void add02();
     void add03();
     void add04();
     void add05();
-    //
-    // adc
-    //
     void adc10();
     void adc11();
     void adc12();
     void adc13();
     void adc14();
     void adc15();
-    //
-    // sub
-    //
-    template <typename T>
-    std::pair<uint16_t, T> do_sub(uint16_t v1, uint16_t v2,
-                                  uint16_t carry_in=0);
     void sub28();
     void sub29();
     void sub2a();
     void sub2b();
     void sub2c();
     void sub2d();
-    //
-    //sbb
-    //
     void sbb18();
     void sbb19();
     void sbb1a();
     void sbb1b();
     void sbb1c();
     void sbb1d();
-    //
-    // inc
-    //
     void inc_dec_fe();
     void incff();
     void inc40_47();
-    //
-    // dec
-    //
     void decff();
     void dec48_4f();
-    //
-    // ascii
-    //
     void aaa37();
     void daa27();
     void aas3f();
     void das2f();
     void aamd4();
-    //
-    // neg
-    //
     void negf6();
     void negf7();
-    //
-    // cmp
-    //
     void cmp38();
     void cmp39();
     void cmp3a();
     void cmp3b();
     void cmp3c();
     void cmp3d();
-    //
-    // mul
-    //
     void mulf6();
     void mulf7();
-    //
-    // imul
-    //
     void imulf6();
     void imulf7();
-    //
-    // int
-    //
     void intcc();
-    //
-    // cbw / cwd
-    //
     void cbw98();
     void cwd99();
-    //
-    // jump
-    //
     void jmpe9();
     void jmpeb();
     void jmpff_intra();
@@ -234,43 +140,29 @@ private:
     void jnbe77();
     void jnp7b();
     void jno71();
-    //
-    // call
-    //
     void calle8();
     void callff_intra();
     void callff_inter();
     void call9a();
-    //
-    // ret
-    //
     void retc3();
     void retc2();
     void retcb();
     void retca();
-    //
-    // iret
-    //
     void iretcf();
-    //
-    // clc
-    //
     void clcf8();
-    //
-    // cmc
-    //
     void cmcf5();
-    //
-    // stc
-    //
     void stcf9();
-    //
-    // cld
-    //
     void cldfc();
+
+    uint8_t fetch_byte();
     template <typename T>
     std::pair<uint16_t, T> do_mul(int32_t v1, int32_t v2);
-    // Helpers
+    template <typename T>
+    std::pair<uint16_t, T> do_add(uint16_t v1, uint16_t v2,
+                                  uint16_t carry_in=0);
+    template <typename T>
+    std::pair<uint16_t, T> do_sub(uint16_t v1, uint16_t v2,
+                                  uint16_t carry_in=0);
     void push_inc_jmp_call_ff();
     void neg_mul_f6();
     void neg_mul_f7();
@@ -323,7 +215,6 @@ size_t EmulatorPimpl::emulate()
 
     opcode = fetch_byte();
     switch (opcode) {
-    // mov
     case 0x88: mov88(); break;
     case 0x89: mov89(); break;
     case 0x8a: mov8a(); break;
@@ -338,114 +229,87 @@ size_t EmulatorPimpl::emulate()
     case 0xa3: mova3(); break;
     case 0x8e: mov8e(); break;
     case 0x8c: mov8c(); break;
-    // push
     case 0xff: push_inc_jmp_call_ff(); break;
     case 0x50 ... 0x57: push50_57(); break;
     case 0x06: // fallthrough
     case 0x0e: // fallthrough
     case 0x16: // fallthrough
     case 0x1e: pushsr(); break;
-    // pop
     case 0x8f: pop8f(); break;
     case 0x58 ... 0x5f: pop58_5f(); break;
     case 0x07: // fallthrough
     case 0x0f: // fallthrough
     case 0x17: // fallthrough
     case 0x1f: popsr(); break;
-    // xchg
     case 0x86: xchg86(); break;
     case 0x87: xchg87(); break;
     case 0x90 ... 0x97: xchg90_97(); break;
-    // in
     case 0xe4: ine4(); break;
     case 0xe5: ine5(); break;
     case 0xec: inec(); break;
     case 0xed: ined(); break;
-    // out
     case 0xe6: oute6(); break;
     case 0xe7: oute7(); break;
     case 0xee: outee(); break;
     case 0xef: outef(); break;
-    // xlat
     case 0xd7: xlatd7(); break;
-    // lea
     case 0x8d: lea8d(); break;
-    // lds
     case 0xc5: ldsc5(); break;
-    // les
     case 0xc4: lesc4(); break;
-    // lahf
     case 0x9f: lahf9f(); break;
-    // sahf
     case 0x9e: sahf9e(); break;
-    // pushf
     case 0x9c: pushf9c(); break;
-    // popf
     case 0x9d: popf9d(); break;
-    // add / adc / sub / sbb / cmp
     case 0x80: add_adc_sub_sbb_cmp_80(); break;
     case 0x81: add_adc_sub_sbb_cmp_81(); break;
     case 0x82: add_adc_sub_sbb_cmp_82(); break;
     case 0x83: add_adc_sub_sbb_cmp_83(); break;
-    // add
     case 0x00: add00(); break;
     case 0x01: add01(); break;
     case 0x02: add02(); break;
     case 0x03: add03(); break;
     case 0x04: add04(); break;
     case 0x05: add05(); break;
-    // adc
     case 0x10: adc10(); break;
     case 0x11: adc11(); break;
     case 0x12: adc12(); break;
     case 0x13: adc13(); break;
     case 0x14: adc14(); break;
     case 0x15: adc15(); break;
-    // sub
     case 0x28: sub28(); break;
     case 0x29: sub29(); break;
     case 0x2a: sub2a(); break;
     case 0x2b: sub2b(); break;
     case 0x2c: sub2c(); break;
     case 0x2d: sub2d(); break;
-    // sbb
     case 0x18: sbb18(); break;
     case 0x19: sbb19(); break;
     case 0x1a: sbb1a(); break;
     case 0x1b: sbb1b(); break;
     case 0x1c: sbb1c(); break;
     case 0x1d: sbb1d(); break;
-    // inc
     case 0xfe: inc_dec_fe(); break;
     case 0x40 ... 0x47: inc40_47(); break;
-    // dec
     case 0x48 ... 0x4f: dec48_4f(); break;
-    // ascii
     case 0x37: aaa37(); break;
     case 0x27: daa27(); break;
     case 0x2f: das2f(); break;
     case 0x3f: aas3f(); break;
     case 0xd4: aamd4(); break;
-    // neg
     case 0xf6: neg_mul_f6(); break;
     case 0xf7: neg_mul_f7(); break;
-    // cmp
     case 0x38: cmp38(); break;
     case 0x39: cmp39(); break;
     case 0x3a: cmp3a(); break;
     case 0x3b: cmp3b(); break;
     case 0x3c: cmp3c(); break;
     case 0x3d: cmp3d(); break;
-    // int
     case 0xcc:
         intcc();
         executed_int3 = true;
         break;
-    // cbw
     case 0x98: cbw98(); break;
-    // cwd
     case 0x99: cwd99(); break;
-    // jmp
     case 0xe9: jmpe9(); break;
     case 0xea: jmpea(); break;
     case 0xeb: jmpeb(); break;
@@ -464,23 +328,16 @@ size_t EmulatorPimpl::emulate()
     case 0x77: jnbe77(); break;
     case 0x7b: jnp7b(); break;
     case 0x71: jno71(); break;
-    // call
     case 0xe8: calle8(); break;
     case 0x9a: call9a(); break;
-    // ret
     case 0xc3: retc3(); break;
     case 0xc2: retc2(); break;
     case 0xcb: retcb(); break;
     case 0xca: retca(); break;
-    // iret
     case 0xcf: iretcf(); break;
-    // clc
     case 0xf8: clcf8(); break;
-    // cmc
     case 0xf5: cmcf5(); break;
-    // stc
     case 0xf9: stcf9(); break;
-    // cld
     case 0xfc: cldfc(); break;
     }
 
