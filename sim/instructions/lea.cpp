7813de30e6545458eade64114e4f737c2734f1ea
Jamie Iles <jamie@jamieiles.com>
2016-05-31 20:57:40 +0100

    sim/lds/les: allow segment override.

--- sim/instructions/lea.cpp
+++ sim/instructions/lea.cpp
@@ -16,7 +16,8 @@ void EmulatorPimpl::ldsc5()
     if (modrm_decoder->rm_type() == OP_REG)
         return;
 
-    auto p32 = get_phys_addr(registers->get(DS), modrm_decoder->effective_address());
+    auto p32 = get_phys_addr(registers->get(get_segment(false)),
+                             modrm_decoder->effective_address());
     auto displacement = mem->read<uint16_t>(p32);
     auto seg = mem->read<uint16_t>(p32 + 2);
 
@@ -33,7 +34,8 @@ void EmulatorPimpl::lesc4()
     if (modrm_decoder->rm_type() == OP_REG)
         return;
 
-    auto p32 = get_phys_addr(registers->get(DS), modrm_decoder->effective_address());
+    auto p32 = get_phys_addr(registers->get(get_segment(false)),
+                             modrm_decoder->effective_address());
     auto displacement = mem->read<uint16_t>(p32);
     auto seg = mem->read<uint16_t>(p32 + 2);
 
