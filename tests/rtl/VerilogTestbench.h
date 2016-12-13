a6200b2d2d1de856ba5d1ccf6429557ae4854cb5
Jamie Iles <jamie@jamieiles.com>
2016-12-13 19:06:50 +0000

    tests: enabled vcd tracing of rtl tests.
    For debug builds, output vcd traces, the filename will be
    TEST_CASE_NAME.TEST_NAME.vcd and output in the build directory.  The
    traces can be viewed with gtkwave.

--- tests/rtl/VerilogTestbench.h
+++ tests/rtl/VerilogTestbench.h
@@ -1,20 +1,66 @@
 #ifndef __VERILOGTESTBENCH_H__
 #define __VERILOGTESTBENCH_H__
 
+#include <verilated_vcd_c.h>
+#include <boost/format.hpp>
+
 template <typename T>
 class VerilogTestbench {
 public:
     VerilogTestbench(T *dut);
+    virtual ~VerilogTestbench();
     void reset();
-    void cycle();
+    virtual void step();
+    virtual void cycle(int count=1);
     T *dut;
+private:
+#ifdef DEBUG
+    void setup_trace();
+    void teardown_trace();
+    VerilatedVcdC tracer;
+    vluint64_t cur_time;
+#endif
 };
 
 template <typename T>
 VerilogTestbench<T>::VerilogTestbench(T *dut)
     : dut(dut)
 {
+    dut->reset = 0;
+    dut->clk = 0;
+#ifdef DEBUG
+    setup_trace();
+#endif
     reset();
+}
+
+#ifdef DEBUG
+template <typename T>
+void VerilogTestbench<T>::setup_trace()
+{
+    cur_time = 0;
+    Verilated::traceEverOn(true);
+    dut->trace(&tracer, 99);
+
+    auto test_info = ::testing::UnitTest::GetInstance()->current_test_info();
+    tracer.open((boost::format("%s.%s.vcd") % test_info->test_case_name() %
+                 test_info->name()).str().c_str());
+}
+
+template <typename T>
+void VerilogTestbench<T>::teardown_trace()
+{
+    tracer.close();
+}
+#endif
+
+template <typename T>
+VerilogTestbench<T>::~VerilogTestbench()
+{
+#ifdef DEBUG
+    teardown_trace();
+#endif
+    dut->final();
 }
 
 template <typename T>
@@ -26,13 +72,21 @@ void VerilogTestbench<T>::reset()
 }
 
 template <typename T>
-void VerilogTestbench<T>::cycle()
+void VerilogTestbench<T>::step()
+{
+    dut->eval();
+    dut->clk = !dut->clk;
+#ifdef DEBUG
+    tracer.dump(cur_time++);
+#endif
+}
+
+template <typename T>
+void VerilogTestbench<T>::cycle(int count)
 {
     /* High+Low clock edges. */
-    for (int i = 0; i < 2; ++i) {
-        dut->eval();
-        dut->clk = ~dut->clk;
-    }
+    for (int i = 0; i < count * 2; ++i)
+        step();
 }
 
 #endif // __VERILOGTESTBENCH_H__
