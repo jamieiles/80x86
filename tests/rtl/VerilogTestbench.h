5a24179e7ffd559d9d7d10cc7793b7a00d383e8c
Jamie Iles <jamie@jamieiles.com>
2016-12-14 23:45:53 +0000

    tests/rtl: support events.
    Rather than have each test manually keep track of time and know when to
    change signals, add support for events that can be deferred or run on a
    clock edge.  Use posedge events for reading the state of a simulation
    and negedge events for setting new variables.  Deferred events can be
    used for things like emulating memories that take cycles to propogate.

--- tests/rtl/VerilogTestbench.h
+++ tests/rtl/VerilogTestbench.h
@@ -3,6 +3,14 @@
 
 #include <verilated_vcd_c.h>
 #include <boost/format.hpp>
+#include <functional>
+#include <map>
+#include <vector>
+
+enum EdgeType {
+    PosEdge,
+    NegEdge,
+};
 
 template <typename T>
 class VerilogTestbench {
@@ -10,16 +18,35 @@ public:
     VerilogTestbench(T *dut);
     virtual ~VerilogTestbench();
     void reset();
+    void at_cycle(vluint64_t cycle_num,
+                  std::function<void()> cb);
+    void after_n_cycles(vluint64_t delta,
+                        std::function<void()> cb)
+    {
+        at_cycle((cur_time / 2) + delta, cb);
+    }
+    void at_edge(EdgeType edge_type, std::function<void()> fn)
+    {
+        edge_events[edge_type].push_back(fn);
+    }
     virtual void step();
     virtual void cycle(int count=1);
+    vluint64_t cur_cycle() const
+    {
+        return cur_time / 2;
+    }
     T *dut;
 private:
+    void run_deferred_events();
+    void run_edge_events(EdgeType edge_type);
 #ifdef DEBUG
     void setup_trace();
     void teardown_trace();
     VerilatedVcdC tracer;
 #endif
     vluint64_t cur_time;
+    std::map<vluint64_t, std::vector<std::function<void()>>> deferred_events;
+    std::map<EdgeType, vector<std::function<void()>>> edge_events;
 };
 
 template <typename T>
@@ -85,9 +112,40 @@ void VerilogTestbench<T>::step()
 template <typename T>
 void VerilogTestbench<T>::cycle(int count)
 {
-    /* High+Low clock edges. */
-    for (int i = 0; i < count * 2; ++i)
+    for (int i = 0; i < count; ++i) {
+        run_deferred_events();
+        /* High clock edge. */
         step();
+        run_edge_events(PosEdge);
+        /* Low clock edge. */
+        step();
+        run_edge_events(NegEdge);
+    }
+}
+
+template <typename T>
+void VerilogTestbench<T>::run_edge_events(EdgeType edge_type)
+{
+    for (auto &e: edge_events[edge_type])
+        e();
+}
+
+template <typename T>
+void VerilogTestbench<T>::run_deferred_events()
+{
+    auto cycle = cur_time / 2;
+
+    for (auto &e: deferred_events[cycle])
+        e();
+}
+
+template <typename T>
+void VerilogTestbench<T>::at_cycle(vluint64_t cycle_num,
+                                   std::function<void()> cb)
+{
+    assert(cycle_num >= cur_time / 2);
+
+    deferred_events[cycle_num].push_back(cb);
 }
 
 #endif // __VERILOGTESTBENCH_H__
