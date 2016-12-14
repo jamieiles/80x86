1282eba1b0d741776a9d6c65e3811ec08bda5bc0
Jamie Iles <jamie@jamieiles.com>
2016-12-14 23:47:12 +0000

    tests/prefetch: update to use new event mechanism.

--- tests/rtl/prefetch.cpp
+++ tests/rtl/prefetch.cpp
@@ -8,15 +8,15 @@
 class PrefetchTestbench : public VerilogTestbench<VPrefetch> {
 public:
     PrefetchTestbench(VPrefetch *dut);
-    virtual void cycle(int count);
     std::vector<uint8_t> fifo_bytes;
     std::map<uint32_t, uint16_t> memory;
     int mem_latency;
-    int mem_cycles_remaining;
+private:
+    bool reading;
 };
 
 PrefetchTestbench::PrefetchTestbench(VPrefetch *dut)
-    : VerilogTestbench<VPrefetch>(dut)
+    : VerilogTestbench<VPrefetch>(dut), mem_latency(0), reading(false)
 {
     dut->reset = 0;
     dut->cs = 0;
@@ -26,37 +26,26 @@ PrefetchTestbench::PrefetchTestbench(VPrefetch *dut)
     dut->mem_access = 0;
     dut->mem_ack = 0;
     dut->mem_data = 0;
-    mem_latency = 1;
-    mem_cycles_remaining = 0;
-    reset();
-}
 
-void PrefetchTestbench::cycle(int count=1)
-{
-    for (auto i = 0; i < count; ++i) {
-        if (!dut->reset && dut->fifo_wr_en)
-            fifo_bytes.push_back(dut->fifo_wr_data & 0xff);
-
-        // High edge
-        VerilogTestbench<VPrefetch>::step();
-
-        dut->eval();
-        dut->mem_ack = 0;
-
-        if (dut->mem_access && mem_cycles_remaining <= 0)
-            mem_cycles_remaining = mem_latency;
-        if (!dut->reset && dut->mem_access && --mem_cycles_remaining == 0) {
-            if (memory.find(dut->mem_address) == memory.end())
-                FAIL() << "no memory at 0x" << std::hex << dut->mem_address;
-            dut->mem_data = memory[dut->mem_address];
-            dut->mem_ack = 1;
+    at_edge(PosEdge, [&]{
+        if (!this->dut->reset && this->dut->fifo_wr_en)
+            fifo_bytes.push_back(this->dut->fifo_wr_data & 0xff);
+    });
+    at_edge(NegEdge, [&]{
+        this->dut->mem_ack = 0;
+        this->dut->mem_data = 0;
+        if (!this->dut->reset && this->dut->mem_access && !reading) {
+            reading = true;
+            if (memory.find(this->dut->mem_address) == memory.end())
+                FAIL() << "no memory at 0x" << std::hex << this->dut->mem_address;
+            after_n_cycles(mem_latency, [&]{
+                this->dut->mem_data = memory[this->dut->mem_address];
+                this->dut->mem_ack = 1;
+                after_n_cycles(1, [&]{ this->dut->mem_ack = 0; reading = false; });
+            });
         }
-
-        // Low edge
-        VerilogTestbench<VPrefetch>::step();
-
-        dut->eval();
-    }
+    });
+    reset();
 }
 
 class PrefetchTestFixture : public ::testing::Test {
@@ -96,13 +85,17 @@ TEST_F(PrefetchTestFixture, fetch_writes_to_fifo)
 
 TEST_F(PrefetchTestFixture, update_ip_resets_fifo)
 {
+    tb.memory[0x000] = 0xf00f;
     tb.memory[0x100] = 0x1234;
+    tb.memory[0x102] = 0x0ff0;
     tb.dut->new_ip = 0x101;
     tb.dut->load_new_ip = 1;
     tb.cycle();
     tb.dut->load_new_ip = 0;
     ASSERT_TRUE(tb.dut->fifo_reset);
     ASSERT_EQ(tb.dut->mem_address, 0x100LU);
+    tb.cycle(4);
+    ASSERT_EQ(tb.fifo_bytes, std::vector<uint8_t>{0x12});
 }
 
 TEST_F(PrefetchTestFixture, odd_fetch_address_writes_one)
@@ -149,16 +142,18 @@ TEST_F(PrefetchTestFixture, address_generation)
 TEST_F(PrefetchTestFixture, new_ip_discards_current_fetch)
 {
     tb.mem_latency = 5;
-    tb.memory[0x0000] = 0xffff;
+    tb.memory[0xdead0] = 0xffff;
     tb.memory[0xdead8] = 0xf00d;
+    tb.memory[0xdeada] = 0xface;
+    tb.memory[0xdeadc] = 0x5555;
     tb.dut->cs = 0xdead;
     tb.dut->new_ip = 0x8;
     tb.cycle(2);
     tb.dut->load_new_ip = 1;
     tb.cycle();
     tb.dut->load_new_ip = 0;
-    tb.cycle(10);
-    ASSERT_EQ(tb.fifo_bytes, (std::vector<uint8_t>{0x0d, 0xf0}));
+    tb.cycle(20);
+    ASSERT_EQ(tb.fifo_bytes, (std::vector<uint8_t>{0x0d, 0xf0, 0xce, 0xfa}));
 }
 
 TEST_F(PrefetchTestFixture, filled_fifo_doesnt_skip)
@@ -166,23 +161,15 @@ TEST_F(PrefetchTestFixture, filled_fifo_doesnt_skip)
     for (uint32_t m = 0; m < 32; m += 2)
         tb.memory[m] = m;
 
-    int pushed = 0;
-    while (pushed != 6) {
+    while (tb.fifo_bytes.size() != 6)
         tb.cycle();
-        if (tb.dut->fifo_wr_en)
-            ++pushed;
-    }
     tb.dut->fifo_full = 1;
     tb.cycle(128);
     tb.dut->fifo_full = 0;
     tb.cycle();
 
-    pushed = 0;
-    while (pushed != 2) {
+    while (tb.fifo_bytes.size() != 8)
         tb.cycle();
-        if (tb.dut->fifo_wr_en)
-            ++pushed;
-    }
 
     ASSERT_EQ(tb.fifo_bytes, (std::vector<uint8_t>{0, 0, 2, 0, 4, 0, 6, 0}));
 }
