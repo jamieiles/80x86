7e4cafacff50fec0c1942614396125563138169b
Jamie Iles <jamie@jamieiles.com>
2016-12-16 01:18:16 +0000

    rtl: add SegmentRegisterFile.
    This continually outputs CS because it needs to be fed to the
    prefetcher.

---
+++ rtl/SegmentRegisterFile.v
@@ -0,0 +1,41 @@
+module SegmentRegisterFile(input logic clk,
+                           input logic reset,
+                           /* Read port. */
+                           input logic [1:0] rd_sel,
+                           output logic [15:0] rd_val,
+                           /* Write port. */
+                           input logic wr_en,
+                           input logic [1:0] wr_sel,
+                           input logic [15:0] wr_val,
+                           /* CS port. */
+                           output logic [15:0] cs);
+
+typedef enum bit [1:0] {
+    ES,
+    CS,
+    SS,
+    DS
+} SR_t;
+
+reg [15:0] registers[4];
+
+integer i;
+
+wire rd_bypass = wr_en && wr_sel == rd_sel;
+reg [15:0] fetched_val;
+assign rd_val = rd_bypass ? wr_val : fetched_val;
+
+assign cs = wr_sel == CS ? wr_val : registers[CS];
+
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        for (i = 0; i < 4; ++i)
+            registers[i] <= 16'b0;
+    else if (wr_en)
+        registers[wr_sel] <= wr_val;
+end
+
+always @(posedge clk)
+    fetched_val <= registers[rd_sel];
+
+endmodule
---
+++ tests/rtl/TestSegmentRegisterFile.cpp
@@ -0,0 +1,74 @@
+#include <gtest/gtest.h>
+#include <VSegmentRegisterFile.h>
+
+#include "VerilogTestbench.h"
+
+class SegmentRegisterFileTestbench : public VerilogTestbench<VSegmentRegisterFile> {
+public:
+    SegmentRegisterFileTestbench(VSegmentRegisterFile *dut);
+    void write_sr(uint8_t sr, uint16_t v);
+    void trigger_read_sr(uint8_t sr);
+    uint16_t get_sr() const;
+};
+
+SegmentRegisterFileTestbench::SegmentRegisterFileTestbench(VSegmentRegisterFile *dut)
+    : VerilogTestbench<VSegmentRegisterFile>(dut)
+{
+}
+
+void SegmentRegisterFileTestbench::write_sr(uint8_t sr, uint16_t v)
+{
+    dut->wr_sel = sr;
+    dut->wr_val = v;
+    dut->wr_en = 1;
+    cycle();
+    dut->wr_en = 0;
+}
+
+void SegmentRegisterFileTestbench::trigger_read_sr(uint8_t sr)
+{
+    dut->rd_sel = sr;
+    cycle();
+}
+
+uint16_t SegmentRegisterFileTestbench::get_sr() const
+{
+    return dut->rd_val;
+}
+
+class SegmentRegisterFileTestFixture : public ::testing::Test {
+public:
+    SegmentRegisterFileTestFixture()
+        : tb(&register_file) {}
+protected:
+    VSegmentRegisterFile register_file;
+    SegmentRegisterFileTestbench tb;
+};
+
+TEST_F(SegmentRegisterFileTestFixture, segment_read_write)
+{
+    for (auto r = 0; r < 4; ++r)
+        tb.write_sr(r, ~r);
+
+    for (auto r = 0; r < 4; ++r) {
+        tb.trigger_read_sr(r);
+        EXPECT_EQ(~r & 0xffff, tb.get_sr());
+    }
+}
+
+TEST_F(SegmentRegisterFileTestFixture, sr_read_during_write)
+{
+    tb.dut->wr_val = 0x1234;
+    tb.dut->wr_en = 1;
+    tb.dut->eval();
+    ASSERT_EQ(0x1234, tb.get_sr());
+}
+
+TEST_F(SegmentRegisterFileTestFixture, cs_output_during_write)
+{
+    tb.dut->wr_val = 0xf00f;
+    tb.dut->wr_en = 1;
+    tb.dut->wr_sel = 1;
+    tb.dut->eval();
+    ASSERT_EQ(0xf00f, tb.dut->cs);
+}
