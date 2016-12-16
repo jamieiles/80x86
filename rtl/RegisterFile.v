3bf8bba46c39889f99dee2c243c7967693b087fc
Jamie Iles <jamie@jamieiles.com>
2016-12-16 00:17:54 +0000

    rtl: initial register file support.

---
+++ rtl/RegisterFile.v
@@ -0,0 +1,79 @@
+module RegisterFile(input logic clk,
+                    input logic reset,
+                    input logic is_8_bit,
+                    /* Read port. */
+                    input logic [2:0] rd_sel[2],
+                    output logic [15:0] rd_val[2],
+                    /* Write port. */
+                    input logic [2:0] wr_sel,
+                    input logic [15:0] wr_val,
+                    input logic wr_en);
+
+typedef enum bit [2:0] {
+    AX,
+    CX,
+    DX,
+    BX,
+    SP,
+    BP,
+    SI,
+    DI
+} GPR16_t;
+
+typedef enum bit [2:0] {
+    AL,
+    CL,
+    DL,
+    BL,
+    AH,
+    CH,
+    DH,
+    BH
+} GPR8_t;
+
+reg [15:0] gprs[8];
+
+wire wr_sel_low_byte = ~wr_sel[2];
+wire [2:0] wr_8_bit_sel = {1'b0, wr_sel[1:0]};
+
+integer i;
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        for (i = 0; i < 8; ++i)
+            gprs[i] <= 16'b0;
+    end else if (wr_en) begin
+        if (is_8_bit) begin
+            if (wr_sel_low_byte)
+                gprs[wr_8_bit_sel][7:0] <= wr_val[7:0];
+            else
+                gprs[wr_8_bit_sel][15:8] <= wr_val[7:0];
+        end else begin
+            gprs[wr_sel] <= wr_val;
+        end
+    end
+end
+
+genvar rd_port;
+
+generate
+for (rd_port = 0; rd_port < 2; ++rd_port) begin: read_port
+    wire rd_sel_low_byte = ~rd_sel[rd_port][2];
+    wire [2:0] rd_8_bit_sel = {1'b0, rd_sel[rd_port][1:0]};
+    reg [15:0] fetched_val;
+    wire bypass = wr_en && wr_sel == rd_sel[rd_port];
+    assign rd_val[rd_port] = bypass ? wr_val : fetched_val;
+
+    always @(posedge clk) begin
+        if (is_8_bit)
+            fetched_val <= {8'b0,
+                            rd_sel_low_byte ?
+                                gprs[rd_8_bit_sel][7:0] :
+                                gprs[rd_8_bit_sel][15:8]};
+        else
+            fetched_val <= gprs[rd_sel[rd_port]];
+    end
+end
+endgenerate
+
+endmodule
---
+++ tests/rtl/TestRegisterFile.cpp
@@ -0,0 +1,136 @@
+#include <gtest/gtest.h>
+#include <VRegisterFile.h>
+
+#include "VerilogTestbench.h"
+
+class RegisterFileTestbench : public VerilogTestbench<VRegisterFile> {
+public:
+    RegisterFileTestbench(VRegisterFile *dut);
+    void write8(uint8_t reg, uint8_t v);
+    void write16(uint8_t reg, uint16_t v);
+    void trigger_read8(int port, uint8_t reg);
+    void trigger_read16(int port, uint8_t reg);
+    uint16_t get_value(int port) const;
+};
+
+RegisterFileTestbench::RegisterFileTestbench(VRegisterFile *dut)
+    : VerilogTestbench<VRegisterFile>(dut)
+{
+}
+
+void RegisterFileTestbench::write8(uint8_t reg, uint8_t v)
+{
+    dut->wr_sel = reg;
+    dut->wr_val = v;
+    dut->wr_en = 1;
+    dut->is_8_bit = 1;
+    cycle();
+    dut->wr_en = 0;
+}
+
+void RegisterFileTestbench::write16(uint8_t reg, uint16_t v)
+{
+    dut->wr_sel = reg;
+    dut->wr_val = v;
+    dut->wr_en = 1;
+    dut->is_8_bit = 0;
+    cycle();
+    dut->wr_en = 0;
+}
+
+void RegisterFileTestbench::trigger_read8(int port, uint8_t reg)
+{
+    dut->rd_sel[port] = reg;
+    dut->is_8_bit = 1;
+    cycle();
+}
+
+void RegisterFileTestbench::trigger_read16(int port, uint8_t reg)
+{
+    dut->rd_sel[port] = reg;
+    dut->is_8_bit = 0;
+    cycle();
+}
+
+uint16_t RegisterFileTestbench::get_value(int port) const
+{
+    return dut->rd_val[port];
+}
+
+class RegisterFileTestFixture : public ::testing::Test {
+public:
+    RegisterFileTestFixture()
+        : tb(&register_file) {}
+protected:
+    VRegisterFile register_file;
+    RegisterFileTestbench tb;
+};
+
+TEST_F(RegisterFileTestFixture, read_write_8_bit)
+{
+    for (auto r = 0; r < 8; ++r)
+        tb.write8(r, r);
+
+    for (auto r = 0; r < 8; ++r) {
+        tb.trigger_read8(0, r);
+        EXPECT_EQ(r, tb.get_value(0));
+    }
+}
+
+TEST_F(RegisterFileTestFixture, read_write_16_bit)
+{
+    for (auto r = 0; r < 8; ++r)
+        tb.write16(r, r | ((r << 8) + 1));
+
+    for (auto r = 0; r < 8; ++r) {
+        tb.trigger_read16(0, r);
+        EXPECT_EQ(r | ((r << 8) + 1), tb.get_value(0));
+    }
+}
+
+TEST_F(RegisterFileTestFixture, reset)
+{
+    for (auto r = 0; r < 8; ++r)
+        tb.write16(r, ~r);
+
+    tb.reset();
+
+    for (auto r = 0; r < 8; ++r) {
+        tb.trigger_read16(0, r);
+        EXPECT_EQ(0, tb.get_value(0));
+    }
+}
+
+TEST_F(RegisterFileTestFixture, read_during_write)
+{
+    tb.dut->wr_en = 1;
+    tb.dut->wr_val = 0x1234;
+    tb.dut->eval();
+    ASSERT_EQ(0x1234, tb.get_value(0));
+}
+
+TEST_F(RegisterFileTestFixture, high_low_16)
+{
+    for (auto r = 0; r < 8; ++r)
+        tb.write16(r, r | (~r << 8));
+
+    for (auto r = 0; r < 4; ++r) {
+        tb.trigger_read8(0, r);
+        EXPECT_EQ(r, tb.get_value(0));
+        tb.trigger_read8(0, r + 4);
+        EXPECT_EQ(~r & 0xff, tb.get_value(0));
+    }
+}
+
+TEST_F(RegisterFileTestFixture, multiple_reads)
+{
+    tb.write16(0, 0xdead);
+    tb.write16(1, 0xbeef);
+
+    tb.dut->rd_sel[0] = 1;
+    tb.dut->rd_sel[1] = 0;
+    tb.cycle();
+
+    EXPECT_EQ(0xbeef, tb.get_value(0));
+    EXPECT_EQ(0xdead, tb.get_value(1));
+}
