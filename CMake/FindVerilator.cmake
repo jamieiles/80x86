9c3efac58a297ff1387a6acf24eeea63da5ca060
Jamie Iles <jamie@jamieiles.com>
2017-01-06 01:14:27 +0000

    verilator: support installations in /usr/local.

--- CMake/FindVerilator.cmake
+++ CMake/FindVerilator.cmake
@@ -1,15 +1,21 @@
 find_path(VERILATOR_INCLUDE_DIR verilated.h
-          HINTS /usr/share/verilator/include)
+          HINTS /usr/share/verilator/include
+          /usr/local/share/verilator/include)
 find_path(VERILATOR_VPI_INCLUDE_DIR vpi_user.h
-          HINTS /usr/share/verilator/include/vltstd)
+          HINTS /usr/share/verilator/include/vltstd
+          /usr/local/share/verilator/include/vltstd)
 find_path(VERILATOR_VCD_INCLUDE_DIR verilated_vcd_c.h
-          HINTS /usr/share/verilator/include/)
+          HINTS /usr/share/verilator/include
+          /usr/local/share/verilator/include)
 find_file(VERILATED_CPP verilated.cpp
-          HINTS /usr/share/verilator/include/)
+          HINTS /usr/share/verilator/include
+          /usr/local/share/verilator/include)
 find_file(VERILATED_COV_CPP verilated_cov.cpp
-          HINTS /usr/share/verilator/include/)
+          HINTS /usr/share/verilator/include
+          /usr/local/share/verilator/include)
 find_file(VERILATED_VCD_CPP verilated_vcd_c.cpp
-          HINTS /usr/share/verilator/include/)
+          HINTS /usr/share/verilator/include
+          /usr/local/share/verilator/include)
 
 if (VERILATOR_INCLUDE_DIR AND EXISTS "${VERILATOR_INCLUDE_DIR}/verilated_config.h")
     file(STRINGS "${VERILATOR_INCLUDE_DIR}/verilated_config.h" VERILATOR_VERSION_STRING_LINE REGEX "^#define[ \t]+VERILATOR_VERSION[ \t]+\".*\"$")
