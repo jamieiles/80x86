e64f9d6d1a2471566ce39773ba4370889a95f289
Jamie Iles <jamie@jamieiles.com>
2017-01-12 00:35:25 +0000

    verilator: add verilog include directories from include_directories().

--- CMake/Verilator.cmake
+++ CMake/Verilator.cmake
@@ -27,12 +27,20 @@ function(verilate toplevel sources)
              ${CMAKE_CURRENT_BINARY_DIR}/V${toplevel}__Trace__Slow.cpp)
     endif()
     set(VERILATED_HEADERS "${VERILATED_HEADERS} ${CMAKE_CURRENT_BINARY_DIR}/V${toplevel}.h" )
+    set(VERILATOR_INCLUDE_ARGS "")
+    get_property(incdirs DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY INCLUDE_DIRECTORIES)
+    foreach(incdir ${incdirs})
+        set(VERILATOR_INCLUDE_ARGS "${VERILATOR_INCLUDE_ARGS} -I${incdir}")
+    endforeach(incdir)
+    separate_arguments(VERILATOR_INCLUDE_ARGS)
     add_custom_command(OUTPUT ${generated}
                        COMMAND verilator -sv -O3 ${sources}
                             -Wall -Wwarn-lint -Wwarn-style
                             -I${CMAKE_CURRENT_SOURCE_DIR} ${VERILATOR_TRACE_FLAGS} ${VERILATOR_COVERAGE_FLAGS}
                             --cc --top-module ${toplevel}
                             --Mdir ${CMAKE_CURRENT_BINARY_DIR}
+                            ${VERILATOR_INCLUDE_ARGS}
+                       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                        DEPENDS ${sources})
     add_library(V${toplevel} STATIC ${generated})
     include_directories(${CMAKE_CURRENT_BINARY_DIR})
