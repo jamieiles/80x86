910624194cb153696698f1e08e86fa4470a75f41
Jamie Iles <jamie@jamieiles.com>
2017-01-14 23:34:27 +0000

    LoadStore: add write support.

--- rtl/LoadStore.v
+++ rtl/LoadStore.v
@@ -6,29 +6,53 @@ module LoadStore(input logic clk,
                  input [15:0] mar_in,
                  // Memory Data Register
                  output [15:0] mdr_out,
+                 input logic write_mdr,
+                 input [15:0] mdr_in,
                  // Memory bus
                  output logic [19:1] m_addr,
                  input logic [15:0] m_data_in,
+                 output logic [15:0] m_data_out,
                  output logic m_access,
                  input logic m_ack,
+                 output logic m_wr_en,
+                 output logic [1:0] m_bytesel,
                  // Control
                  input logic start,
                  input logic is_8bit,
-                 output logic complete
-                 );
+                 input logic wr_en,
+                 output logic complete);
 
 // verilator lint_off UNUSED
 reg [15:0] mar;
 // verilator lint_on UNUSED
 reg [15:0] mdr;
 
-assign m_access = (start | fetching | fetch_second) & ~complete & ~m_ack;
+assign m_access = (start | fetching | second_byte) & ~complete & ~m_ack;
 assign m_addr = {segment, 3'b0} + {3'b0, mar[15:1]};
+assign m_wr_en = wr_en;
 assign mdr_out = mdr;
+
+always_comb begin
+    if (unaligned && !second_byte)
+        m_data_out = {mdr[7:0], 8'b0};
+    else if (unaligned && second_byte)
+        m_data_out = {8'b0, mdr[15:8]};
+    else
+        m_data_out = mdr;
+end
+
+always_comb begin
+    if (!is_8bit && !unaligned)
+        m_bytesel = 2'b11;
+    else if (unaligned && !second_byte)
+        m_bytesel = 2'b10;
+    else
+        m_bytesel = 2'b01;
+end
 
 reg unaligned;
 reg fetching;
-reg fetch_second;
+reg second_byte;
 
 always_ff @(posedge clk or posedge reset)
     fetching <= start ? 1'b1 : reset || complete ? 1'b0 : fetching;
@@ -39,7 +63,7 @@ always_ff @(posedge clk or posedge reset)
         write_mar ? mar_in : mar;
 
 always_ff @(posedge clk)
-    complete <= m_ack && (is_8bit || (unaligned && fetch_second) || !unaligned);
+    complete <= m_ack && (is_8bit || (unaligned && second_byte) || !unaligned);
 
 always_ff @(posedge clk or posedge reset)
     if (reset)
@@ -48,21 +72,22 @@ always_ff @(posedge clk or posedge reset)
         unaligned <= mar[0];
 
 always_ff @(posedge clk or posedge reset)
-    if (m_ack && unaligned && !fetch_second && !is_8bit)
-        fetch_second <= 1'b1;
+    if (m_ack && unaligned && !second_byte && !is_8bit)
+        second_byte <= 1'b1;
     else if (reset || (start && !fetching) || complete)
-        fetch_second <= 1'b0;
+        second_byte <= 1'b0;
 
 always_ff @(posedge clk or posedge reset) begin
-    if (reset || (start && !fetching)) begin
+    if (reset || (start && !fetching && !wr_en))
         mdr <= 16'b0;
-    end else if (m_ack && !unaligned) begin
+    else if (write_mdr)
+        mdr <= mdr_in;
+    else if (!wr_en && m_ack && !unaligned)
         mdr <= is_8bit ? {8'b0, m_data_in[7:0]} : m_data_in;
-    end else if (m_ack && unaligned && !fetch_second) begin
+    else if (!wr_en && m_ack && unaligned && !second_byte)
         mdr[7:0] <= unaligned ? m_data_in[15:8] : m_data_in[7:0];
-    end else if (m_ack && unaligned && fetch_second) begin
+    else if (!wr_en && m_ack && unaligned && second_byte)
         mdr[15:8] <= unaligned ? m_data_in[7:0] : m_data_in[15:8];
-    end
 end
 
 endmodule
--- tests/rtl/TestLoadStore.cpp
+++ tests/rtl/TestLoadStore.cpp
@@ -10,6 +10,11 @@ using physaddr = uint32_t;
 class LoadStoreTestbench : public VerilogTestbench<VLoadStore>,
     public ::testing::Test {
 public:
+    enum MemWidth {
+        WIDTH_8,
+        WIDTH_16
+    };
+
     LoadStoreTestbench();
     void add_memory(physaddr start, const std::vector<uint8_t> &bytes);
     std::vector<uint16_t> get_read_values() const
@@ -28,15 +33,34 @@ public:
         });
         cycle();
     }
-    enum MemWidth {
-        WIDTH_8,
-        WIDTH_16
-    };
-    void access(MemWidth width)
+    void write_mdr(physaddr address)
+    {
+        after_n_cycles(0, [&]{
+            this->dut.mdr_in = address;
+            this->dut.write_mdr = 1;
+            after_n_cycles(1, [&]{
+                this->dut.write_mdr = 0;
+                this->dut.mdr_in = 0;
+            });
+        });
+        cycle();
+    }
+    void read(MemWidth width)
+    {
+        access(width, false);
+    }
+    void write(MemWidth width)
+    {
+        access(width, true);
+    }
+    std::map<physaddr, uint8_t> mem_bytes;
+private:
+    void access(MemWidth width, bool write)
     {
         after_n_cycles(0, [&]{
             this->dut.is_8bit = width == WIDTH_8;
             this->dut.start = 1;
+            this->dut.wr_en = write;
             after_n_cycles(1, [&]{
                 this->dut.start = 0;
             });
@@ -49,23 +73,24 @@ public:
 
         FAIL() << "failed to complete memory operation" << std::endl;
     }
-private:
-    std::map<physaddr, uint8_t> mem_bytes;
     std::vector<uint16_t> read_values;
     bool reading;
+    bool writing;
 };
 
 LoadStoreTestbench::LoadStoreTestbench()
-    : reading(false)
+    : reading(false),
+    writing(false)
 {
     reset();
 
     periodic(ClockCapture, [&]{
-        if (!this->dut.reset && this->dut.complete)
+        if (!this->dut.reset && this->dut.complete && !this->dut.wr_en)
             read_values.push_back(this->dut.mdr_out);
     });
     periodic(ClockSetup, [&]{
-        if (!this->dut.reset && this->dut.m_access && !reading) {
+        if (!this->dut.reset && this->dut.m_access && !reading &&
+            !this->dut.wr_en) {
             reading = true;
             if (mem_bytes.find(this->dut.m_addr << 1) == mem_bytes.end())
                 FAIL() << "no memory at 0x" << std::hex << this->dut.m_addr;
@@ -77,6 +102,25 @@ LoadStoreTestbench::LoadStoreTestbench()
                 after_n_cycles(1, [&]{
                     this->dut.m_ack = 0;
                     reading = false;
+                });
+            });
+        }
+    });
+    periodic(ClockSetup, [&]{
+        if (!this->dut.reset && this->dut.m_access && !writing &&
+            this->dut.wr_en) {
+            writing = true;
+            after_n_cycles(4, [&]{
+                if (this->dut.m_bytesel & 0x1)
+                    this->mem_bytes[this->dut.m_addr << 1] =
+                        this->dut.m_data_out & 0xff;
+                if (this->dut.m_bytesel & 0x2)
+                    this->mem_bytes[(this->dut.m_addr << 1) + 1] =
+                        (this->dut.m_data_out >> 8) & 0xff;
+                this->dut.m_ack = 1;
+                after_n_cycles(1, [&]{
+                    this->dut.m_ack = 0;
+                    writing = false;
                 });
             });
         }
@@ -119,7 +163,7 @@ TEST_P(LoadTest, LoadMemory)
     add_memory(0xff, {0xff, 0x55, 0xaa, 0xff});
 
     write_mar(params.mar_value);
-    access(params.width);
+    read(params.width);
 
     ASSERT_EQ(get_read_values(), params.values);
 }
@@ -129,4 +173,45 @@ INSTANTIATE_TEST_CASE_P(Load, LoadTest,
         LoadParams{0x101U, LoadStoreTestbench::MemWidth::WIDTH_16, {0xffaa}},
         LoadParams{0x100U, LoadStoreTestbench::MemWidth::WIDTH_8, {0x55}},
         LoadParams{0x101U, LoadStoreTestbench::MemWidth::WIDTH_8, {0xaa}}
+    ));
+
+struct StoreParams {
+    physaddr mar_value;
+    uint16_t mdr_value;
+    LoadStoreTestbench::MemWidth width;
+    std::map<physaddr, uint8_t> expected_mem;
+};
+
+class StoreTest : public LoadStoreTestbench,
+    public ::testing::WithParamInterface<StoreParams> {
+};
+
+TEST_P(StoreTest, StoreMemory)
+{
+    auto params = GetParam();
+
+    write_mar(params.mar_value);
+    write_mdr(params.mdr_value);
+
+    write(params.width);
+
+    ASSERT_TRUE(params.expected_mem.size() == mem_bytes.size() &&
+                std::equal(params.expected_mem.begin(),
+                           params.expected_mem.end(),
+                           mem_bytes.begin()));
+}
+INSTANTIATE_TEST_CASE_P(Store, StoreTest,
+    ::testing::Values(
+        StoreParams{0x100U, 0xaa55,
+                    LoadStoreTestbench::MemWidth::WIDTH_16,
+                    {{0x100, 0x55}, {0x101, 0xaa}}},
+        StoreParams{0x101U, 0xaa55,
+                    LoadStoreTestbench::MemWidth::WIDTH_16,
+                    {{0x101, 0x55}, {0x102, 0xaa}}},
+        StoreParams{0x100U, 0xff55,
+                    LoadStoreTestbench::MemWidth::WIDTH_8,
+                    {{0x100, 0x55}}},
+        StoreParams{0x101U, 0xff55,
+                    LoadStoreTestbench::MemWidth::WIDTH_8,
+                    {{0x101, 0x55}}}
     ));
