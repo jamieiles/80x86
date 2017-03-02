39b1b0e91c03cf1d38b474e627cc4f4943b84bbb
Jamie Iles <jamie@jamieiles.com>
2017-03-02 22:46:08 +0000

    microcode: stall on empty fifo.

--- tests/rtl/TestMicrocode.cpp
+++ tests/rtl/TestMicrocode.cpp
@@ -19,19 +19,25 @@ public:
     {
         return this->dut.get_microcode_address();
     }
+    bool underflowed;
 private:
     std::deque<uint8_t> stream;
 };
 
 MicrocodeTestbench::MicrocodeTestbench()
+    : underflowed(false), stream({})
 {
+    dut.fifo_empty = stream.size() == 0;
+
+    reset();
+
     svSetScope(svGetScopeFromName("TOP.Microcode"));
 
     periodic(ClockSetup, [&]{
+        this->dut.fifo_empty = this->stream.size() == 0;
         if (!this->dut.reset && this->dut.fifo_rd_en &&
             this->stream.size() > 0) {
             after_n_cycles(0, [&]{
-                this->dut.fifo_empty = this->stream.size() == 0;
                 this->dut.fifo_rd_data = this->stream.size() > 0 ?
                     this->stream[0] : 0;
                 this->stream.pop_front();
@@ -39,7 +45,7 @@ MicrocodeTestbench::MicrocodeTestbench()
         }
         if (!this->dut.reset && this->dut.fifo_rd_en &&
             this->stream.size() == 0)
-            FAIL() << "fifo underflow" << std::endl;
+            underflowed = true;
     });
 }
 
@@ -50,10 +56,24 @@ TEST_F(MicrocodeTestbench, JumpOpcode)
     ASSERT_EQ(current_address(), 0x100);
 
     for (int i = 0; i < 16; ++i) {
+        if (underflowed)
+            FAIL() << "fifo underflow" << std::endl;
         if (current_address() == 0x30)
             return;
         cycle();
     }
 
     FAIL() << "Didn't hit 0x30 opcode" << std::endl;
+}
+
+TEST_F(MicrocodeTestbench, Stall)
+{
+    cycle();
+
+    for (int i = 0; i < 32; ++i) {
+        cycle();
+        EXPECT_EQ(0x100, current_address());
+    }
+
+    ASSERT_FALSE(underflowed);
 }
