d36abdf1e15b956121f515d2ac296ecbd25c9faf
Jamie Iles <jamie@jamieiles.com>
2017-03-06 22:00:23 +0000

    rtl/IP: provide a mechanism to set/get the IP.

--- rtl/IP.v
+++ rtl/IP.v
@@ -5,14 +5,33 @@ module IP(input logic clk,
           input logic [15:0] wr_val,
           output logic [15:0] val);
 
-reg [15:0] val;
+// verilator lint_off BLKANDNBLK
+reg [15:0] cur_val;
+// verilator lint_on BLKANDNBLK
+assign val = cur_val;
 
 always @(posedge clk or posedge reset)
     if (reset)
-        val <= 16'b0;
+        cur_val <= 16'b0;
     else if (wr_en)
-        val <= wr_val;
+        cur_val <= wr_val;
     else if (inc)
-        val <= val + 1'b1;
+        cur_val <= cur_val + 1'b1;
+
+`ifdef verilator
+export "DPI-C" function get_ip;
+
+function [15:0] get_ip;
+    get_ip = cur_val;
+endfunction
+
+export "DPI-C" function set_ip;
+
+function set_ip;
+    input int new_val;
+    cur_val = new_val[15:0];
+    set_ip = 0;
+endfunction
+`endif
 
 endmodule
