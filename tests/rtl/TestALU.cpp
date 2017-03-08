e0d6664089974b8c94f0084c628301d0b0206f31
Jamie Iles <jamie@jamieiles.com>
2017-03-08 23:36:28 +0000

    microcode: generate C++ headers for definitions.

--- tests/rtl/TestALU.cpp
+++ tests/rtl/TestALU.cpp
@@ -2,6 +2,7 @@
 #include <VALU.h>
 
 #include "VerilogTestbench.h"
+#include "MicrocodeTypes.h"
 
 class ALUTestFixture : public ::testing::Test {
 protected:
@@ -13,11 +14,11 @@ TEST_F(ALUTestFixture, Select)
     dut.a = 0xaaaa;
     dut.b = 0xbbbb;
 
-    dut.op = 2;
+    dut.op = ALUOp_SELA;
     dut.eval();
     ASSERT_EQ(dut.out, 0xaaaa);
 
-    dut.op = 3;
+    dut.op = ALUOp_SELB;
     dut.eval();
     ASSERT_EQ(dut.out, 0xbbbb);
 }
