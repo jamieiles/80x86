68e256012c26aa834144c7b08394a03740c2af6f
Jamie Iles <jamie@jamieiles.com>
2017-03-17 01:08:46 +0000

    python: make RTLCPU available to Python scripts.

--- python/py8086sim/Cpu.cpp
+++ python/py8086sim/Cpu.cpp
@@ -3,8 +3,11 @@
 
 #include "CPU.h"
 #include "RegisterFile.h"
+#include "RTLCPU.h"
 
 using namespace boost::python;
+
+typedef RTLCPU<true> rtlcpu;
 
 BOOST_PYTHON_MODULE(Cpu)
 {
@@ -25,6 +28,23 @@ BOOST_PYTHON_MODULE(Cpu)
         .def("write_flags", &SoftwareCPU::write_flags)
         .def("step", &SoftwareCPU::step)
         .def("has_trapped", &SoftwareCPU::has_trapped);
+    class_<rtlcpu, boost::noncopyable>("RTLCPU", init<const std::string &>("py8086sim-rtl"))
+        .def("write_reg", &rtlcpu::write_reg)
+        .def("read_reg", &rtlcpu::read_reg)
+        .def("write_mem8", &rtlcpu::write_mem<uint8_t>)
+        .def("write_mem16", &rtlcpu::write_mem<uint16_t>)
+        .def("write_mem32", &rtlcpu::write_mem<uint32_t>)
+        .def("read_mem8", &rtlcpu::read_mem<uint8_t>)
+        .def("read_mem16", &rtlcpu::read_mem<uint16_t>)
+        .def("read_mem32", &rtlcpu::read_mem<uint32_t>)
+        .def("write_io8", &rtlcpu::write_io<uint8_t>)
+        .def("write_io16", &rtlcpu::write_io<uint16_t>)
+        .def("read_io8", &rtlcpu::read_io<uint8_t>)
+        .def("read_io16", &rtlcpu::read_io<uint16_t>)
+        .def("read_flags", &rtlcpu::read_flags)
+        .def("write_flags", &rtlcpu::write_flags)
+        .def("step", &rtlcpu::step)
+        .def("has_trapped", &rtlcpu::has_trapped);
     enum_<GPR>("GPR")
         .value("AX", AX)
         .value("CX", CX)
