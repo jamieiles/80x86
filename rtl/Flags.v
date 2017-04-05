c506557c2bac01fad40846cfc94fb4d523ea6ac8
Jamie Iles <jamie@jamieiles.com>
2017-04-05 21:43:01 +0100

    rtl/Flags: remove inferred latches for FPGA synthesis.

--- rtl/Flags.v
+++ rtl/Flags.v
@@ -6,35 +6,32 @@ module Flags(input logic clk,
              input logic [8:0] update_flags,
              output logic [15:0] flags_out);
 
-localparam FLAGS_RESET = 16'h0002;
+reg C, P, A, Z, S, T, I, D, O;
 
-// verilator lint_off BLKANDNBLK
-reg [15:0] flags;
-// verilator lint_on BLKANDNBLK
-
-assign flags_out = flags;
+assign flags_out = {4'b0, O, D, I, T, S, Z, 1'b0, A, 1'b0, P, 1'b1, C};
 
 always_ff @(posedge clk or posedge reset) begin
-    flags[CF_IDX] <=
-        update_flags[UpdateFlags_CF] ? flags_in[CF_IDX] : flags[CF_IDX];
-    flags[PF_IDX] <=
-        update_flags[UpdateFlags_PF] ? flags_in[PF_IDX] : flags[PF_IDX];
-    flags[AF_IDX] <=
-        update_flags[UpdateFlags_AF] ? flags_in[AF_IDX] : flags[AF_IDX];
-    flags[ZF_IDX] <=
-        update_flags[UpdateFlags_ZF] ? flags_in[ZF_IDX] : flags[ZF_IDX];
-    flags[SF_IDX] <=
-        update_flags[UpdateFlags_SF] ? flags_in[SF_IDX] : flags[SF_IDX];
-    flags[TF_IDX] <=
-        update_flags[UpdateFlags_TF] ? flags_in[TF_IDX] : flags[TF_IDX];
-    flags[IF_IDX] <=
-        update_flags[UpdateFlags_IF] ? flags_in[IF_IDX] : flags[IF_IDX];
-    flags[DF_IDX] <=
-        update_flags[UpdateFlags_DF] ? flags_in[DF_IDX] : flags[DF_IDX];
-    flags[OF_IDX] <=
-        update_flags[UpdateFlags_OF] ? flags_in[OF_IDX] : flags[OF_IDX];
-    if (reset)
-        flags <= FLAGS_RESET;
+    if (reset) begin
+        C <= 1'b0;
+        P <= 1'b0;
+        A <= 1'b0;
+        Z <= 1'b0;
+        S <= 1'b0;
+        T <= 1'b0;
+        I <= 1'b0;
+        D <= 1'b0;
+        O <= 1'b0;
+    end else begin
+        C <= update_flags[UpdateFlags_CF] ? flags_in[CF_IDX] : C;
+        P <= update_flags[UpdateFlags_PF] ? flags_in[PF_IDX] : P;
+        A <= update_flags[UpdateFlags_AF] ? flags_in[AF_IDX] : A;
+        Z <= update_flags[UpdateFlags_ZF] ? flags_in[ZF_IDX] : Z;
+        S <= update_flags[UpdateFlags_SF] ? flags_in[SF_IDX] : S;
+        T <= update_flags[UpdateFlags_TF] ? flags_in[TF_IDX] : T;
+        I <= update_flags[UpdateFlags_IF] ? flags_in[IF_IDX] : I;
+        D <= update_flags[UpdateFlags_DF] ? flags_in[DF_IDX] : D;
+        O <= update_flags[UpdateFlags_OF] ? flags_in[OF_IDX] : O;
+    end
 end
 
 endmodule
