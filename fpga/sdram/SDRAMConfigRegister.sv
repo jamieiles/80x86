1ffc7787299c388591e99b2311687a22cc91af8c
Jamie Iles <jamie@jamieiles.com>
2017-04-26 23:28:47 +0100

    rtl/SDRAMConfigRegister: don't output data on writes.

--- fpga/sdram/SDRAMConfigRegister.sv
+++ fpga/sdram/SDRAMConfigRegister.sv
@@ -1,12 +1,14 @@
 module SDRAMConfigRegister(input logic clk,
                            input logic cs,
                            output logic [15:0] data_m_data_out,
+                           input logic data_m_wr_en,
                            input logic data_m_access,
                            output logic data_m_ack,
                            input logic config_done);
 
 always_ff @(posedge clk) begin
-    data_m_data_out <= cs ? {15'b0, config_done} : 16'b0;
+    data_m_data_out <= data_m_access && cs && !data_m_wr_en ?
+        {15'b0, config_done} : 16'b0;
     data_m_ack <= data_m_access && cs;
 end
 
