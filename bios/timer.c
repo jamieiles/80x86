25c1e248a464129e044e7d6a77478ec8a30373b6
Jamie Iles <jamie@jamieiles.com>
2017-08-24 18:47:06 +0100

    bios: rework timer interrupt.
    The timer interrupt was switching stacks like a normal BIOS call, but
    this was a problem when running qbasic, because it looks like the
    application was hooking the interrupt itself and then making a nested
    BIOS call which would result in clobbering an existing stack.
    
    Rather than supporting nested interrupts, just use the existing stack
    for the timer interrupt - it only requires a couple of pushes.

--- bios/timer.c
+++ bios/timer.c
@@ -2,19 +2,17 @@
 #include "io.h"
 #include "timer.h"
 
-static unsigned long time_count;
-
 static void timer_services(struct callregs *regs)
 {
     switch (regs->ax.h) {
     case 0x0:
-        regs->cx.x = time_count >> 16;
-        regs->dx.x = time_count;
+        regs->dx.x = readw(0x40, (void *)0x6c);
+        regs->cx.x = readw(0x40, (void *)0x6e);
         regs->flags &= ~CF;
         break;
     case 0x1:
-        time_count = (regs->dx.x |
-                      ((unsigned long)regs->cx.x << 16));
+        writew(0x40, 0x6c, regs->dx.x);
+        writew(0x40, 0x6e, regs->cx.x);
         regs->flags &= ~CF;
         break;
     default:
@@ -23,24 +21,9 @@ static void timer_services(struct callregs *regs)
 }
 VECTOR(0x1a, timer_services);
 
-static void timer_irq(struct callregs *regs)
-{
-    ++time_count;
-
-    memcpy_seg(0x40, (void *)0x006c, get_cs(), &time_count,
-               sizeof(time_count));
-
-    asm volatile("int $0x1c");
-
-    (void)inw(0xffee);
-}
-VECTOR(0x08, timer_irq);
-
 void init_timer(void)
 {
     outb(0xfff5, 8);
     outb(0xfff4, 1);
     outw(0xffee, (1 << 15) | 55);
-
-    time_count = 0;
 }
