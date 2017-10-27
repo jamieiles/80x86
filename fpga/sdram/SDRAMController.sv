b029ecc2801f8f56389f8f7176f38c249a2b67cf
Jamie Iles <jamie@jamieiles.com>
2017-10-27 18:17:58 +0100

    Relicense to GPLv3.

--- fpga/sdram/SDRAMController.sv
+++ fpga/sdram/SDRAMController.sv
@@ -1,3 +1,20 @@
+// Copyright Jamie Iles, 2017
+//
+// This file is part of s80x86.
+//
+// s80x86 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// s80x86 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with s80x86.  If not, see <http://www.gnu.org/licenses/>.
+
 /*
  * Simple SDR SDRAM controller for 4Mx16x4 devices, e.g. ISSI IS45S16160G
  * (32MB) or 64MB variants.
